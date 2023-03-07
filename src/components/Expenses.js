48a7b304953cfbad4f6127ed9e3ae7e812b03a6d
Musharraf <musharrafsayyad1@gmail.com>
2023-03-07 15:05:14 +0530

    Card.js

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,14 +1,15 @@
+import Card from "./Card";
 import "./Expenses.css";
 import ExpenseItem from "./ExpensesItem";
 const Expenses =(props)=>{
-return <div className="expenses">
+return <Card className="expenses">
 <ExpenseItem date={ props.expenses[0].date} title={ props.expenses[0].title} amount={ props.expenses[0].amount}/>
 <ExpenseItem date={ props.expenses[1].date} title={ props.expenses[1].title} amount={ props.expenses[1].amount}/>
 <ExpenseItem date={ props.expenses[2].date} title={ props.expenses[2].title} amount={ props.expenses[2].amount}/>
 <ExpenseItem date={ props.expenses[3].date} title={ props.expenses[3].title} amount={ props.expenses[3].amount}/>
 <ExpenseItem date={ props.expenses[4].date} title={ props.expenses[4].title} amount={ props.expenses[4].amount}/>
 <ExpenseItem date={ props.expenses[5].date} title={ props.expenses[5].title} amount={ props.expenses[5].amount}/>
-  </div>
+  </Card>
 }
 
 export default Expenses;
